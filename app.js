9ceb17959e54645a71e655182a6db73c724fb1ab
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-06-09 12:05:18 -0400

    adding moving functions

--- app.js
+++ app.js
@@ -95,4 +95,16 @@ document.addEventListener('DOMContentLoaded', () => {
             gameOver()
         }
     }
+
+    function moveLeft() {
+      undraw()
+      const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+      if(!isAtLeftEdge) currentPosition -=1
+      if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition +=1
+      }
+      draw()
+    }
+
+    
 })
